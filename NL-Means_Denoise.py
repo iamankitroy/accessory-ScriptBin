06c5e231cd7f01c6b310e5468c0a85f8f2924e35
iamankitroy <intellect.ankit@gmail.com>
2022-11-24 15:12:01 +0100

    Cleaned up script.

--- NL-Means_Denoise.py
+++ NL-Means_Denoise.py
@@ -8,7 +8,6 @@ import sys
 import numpy as np
 from skimage import img_as_float, io
 from skimage.restoration import denoise_nl_means, estimate_sigma
-import multiprocessing as mp
 
 # Read input image file
 def readImg(filename):
@@ -16,52 +15,47 @@ def readImg(filename):
     return img
 
 # Denoise image
-def denoiseImg(img, frame, patch_size=5, patch_distance=6):
+def denoiseImg(img):
+    sigma_est = np.mean(estimate_sigma(img, channel_axis=None))             # Estimate noise standard deviation
 
-    # Estimate noise standard deviation
-    sigma_est = np.mean(estimate_sigma(img, channel_axis=None))
     # Apply NL-Means denoising
-    denoise = denoise_nl_means(img, h=1.15 * sigma_est, fast_mode=False, patch_size=patch_size, patch_distance=patch_distance)
-
-    # report progress
-    print(f'Frame: {frame+1:>5} denoised!')
+    denoise = denoise_nl_means(img, h=1.15 * sigma_est, fast_mode=False, patch_size=5, patch_distance=6)
 
     return denoise
 
+# Progress bar
+def progress_bar(current, total, bar_length=20):
+    fraction = current/total
+
+    arrow = int(fraction*bar_length-1) * '-' + '>'
+    padding = int(bar_length-len(arrow)) * ' '
+
+    ending = '\n' if current == total else '\r'
+
+    print(f'Progress: [{arrow}{padding}] {int(fraction*100)}% | Frame {current} of {total}', end=ending)
+
 # Main function
 def main():
-
     px_bitsize = 16                         # pixel bit size
-    patch_size = 9                          # patch size for NL-Means
-    patch_distance = 6                      # patch distance for NL-Means
 
     filename = sys.argv[1]                  # image file name
     img = readImg(filename)                 # read image data
-    
-    # store processes for parallelization
-    processes = [(img[t], t, patch_size, patch_distance) for t in range(len(img))]
+    cleaned_img = np.zeros(np.shape(img))   # store cleaned image
 
-    # start multiprocessing
-    pool = mp.Pool(mp.cpu_count())
-    results = pool.starmap(denoiseImg, processes)
-    pool.close
-
-    # denoised image
-    cleaned_img = np.array(results)
+    # denoise every frame of timeseries
+    for t in range(len(img)):
+        denoise = denoiseImg(img[t])        # denoise frame
+        cleaned_img[t] = denoise            # store denoised frame
+        progress_bar(t+1, len(img))         # print progress
     
-    # save output file
-    outname = f"{'.'.join(filename.split('.')[:-1])}_cleaned_v2.tif"
+    # output file name
+    outname = f"{'.'.join(filename.split('.')[:-1])}_cleaned.tif"
     io.imsave(outname, np.uint16(cleaned_img * (2**px_bitsize - 1)))
 
-# Run main
-if __name__ == '__main__':
-    main()
+main()
 
 # Ankit Roy
 # 22nd November, 2022
 # 23rd November, 2022
-#       -->     Removing CLAHE function.
-#       -->     Adding progress bar.
-# 24th November, 2022
-#       -->     Now runs parallel. Each frame is processed by a different CPU.
-#       -->     Removed progress bar, instead reported frames denoised.
+#       -->     Removing CLAHE function
+#       -->     Adding progress bar
